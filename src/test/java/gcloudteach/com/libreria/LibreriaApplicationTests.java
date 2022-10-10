15bf6787d2919f194af9c2c835b1fd36e9ce4763
jpcuencat <49325128+jpcuencat@users.noreply.github.com>
2022-10-10 17:19:16 -0500

    Primeros Archivos

---
+++ src/test/java/gcloudteach/com/libreria/LibreriaApplicationTests.java
@@ -0,0 +1,13 @@
+package gcloudteach.com.libreria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibreriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
