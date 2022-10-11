3a04b47dcac069a905c97575c43087eae8e65884
jpcuencat <49325128+jpcuencat@users.noreply.github.com>
2022-10-10 20:43:26 -0500

    Conexion Exitosa

--- src/main/java/gcloudteach/com/libreria/LibreriaApplication.java
+++ src/main/java/gcloudteach/com/libreria/LibreriaApplication.java
@@ -1,13 +1,29 @@
 package gcloudteach.com.libreria;
 
+import java.nio.file.Path;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.cassandra.CqlSessionBuilderCustomizer;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+
+import connection.DataStaxAstraProperties;
+
+
 
 @SpringBootApplication
+@EnableConfigurationProperties(DataStaxAstraProperties.class)
 public class LibreriaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LibreriaApplication.class, args);
 	}
+
+	@Bean
+    public CqlSessionBuilderCustomizer sessionBuilderCustomizer(DataStaxAstraProperties astraProperties) {
+        Path bundle = astraProperties.getSecureConnectBundle().toPath();
+        return builder -> builder.withCloudSecureConnectBundle(bundle);
+    }
 
 }
