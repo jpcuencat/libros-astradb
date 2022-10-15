2c158b2e10be06d08f84b1555f23041b5ce8b2eb
jpcuencat <49325128+jpcuencat@users.noreply.github.com>
2022-10-14 21:12:40 -0500

    insertando authores

--- src/main/java/gcloudteach/com/libreria/LibreriaApplication.java
+++ src/main/java/gcloudteach/com/libreria/LibreriaApplication.java
@@ -1,10 +1,17 @@
 package gcloudteach.com.libreria;
 
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
 
 import javax.annotation.PostConstruct;
 
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.cassandra.CqlSessionBuilderCustomizer;
@@ -13,10 +20,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.cassandra.repository.config.EnableCassandraRepositories;
 
+
+
 import author.Author;
 import connection.DataStaxAstraProperties;
-
-
 
 @SpringBootApplication
 @Configuration
@@ -24,36 +31,73 @@ import connection.DataStaxAstraProperties;
 
 @EnableConfigurationProperties(DataStaxAstraProperties.class)
 public class LibreriaApplication {
-    
+
     // private AuthorRepo authorRepo;
     // @Autowired
-    //     public void setRepo(AuthorRepo authorRepo) {
-    //         this.authorRepo = authorRepo;
-    //     }
-    
+    // public void setRepo(AuthorRepo authorRepo) {
+    // this.authorRepo = authorRepo;
+    // }
 
-    @Autowired 
+    @Autowired
     private AuthorRep authorRepo;
-    
-	public static void main(String[] args) {
-		SpringApplication.run(LibreriaApplication.class, args);
-	}
 
+    @Value("${datadump.location.author}")
+    private String authorDumpLocation;
 
-    @PostConstruct
-    public void start(){
+    @Value("${datadump.location.works}")
+    private String worksDumpLocation;
 
-        System.out.println("Hasta aqui aplicacion exitosa");
-        Author author = new Author();
-         author.setId("id");        
-         author.setName("Name");
-         author.setPersonalName("personal");
-        authorRepo.save(author);
+    public static void main(String[] args) {
+        SpringApplication.run(LibreriaApplication.class, args);
+    }
+
+    private void initAuthors() {
+        Path path = Paths.get(authorDumpLocation); 
+     //   System.out.println("Hasta aqui aplicacion exitosa" + authorDumpLocation);
+        try (Stream<String> lines = Files.lines(path)) { 
+            lines.forEach(line -> {
+                // Read and parse the line 
+                String jsonString = line.substring(line.indexOf("{")); 
+                try {
+                JSONObject jsonObject = new JSONObject(jsonString); 
+                // Construct Author object 
+                
+                Author author = new Author(); 
+                author.setName(jsonObject.optString("name")); 
+                author.setPersonalName(jsonObject.optString("personal_name")); 
+                author.setId(jsonObject.optString("key").replace("/authors/",""));
+                // Persist using Repository 
+                System.out.println("Escribiendo author: " + author.getName() + "...");
+                authorRepo.save(author);
+                } catch(JSONException e){
+                    e.printStackTrace();
+                }
+            });
+         }catch (IOException e) {
+                 e.printStackTrace();
+         }
+    }
+
+    private void initWorks() {
 
     }
 
+    @PostConstruct
+    public void start() {
 
-	@Bean
+        // System.out.println("Hasta aqui aplicacion exitosa");
+        // Author author = new Author();
+        // author.setId("id");
+        // author.setName("Name");
+        // author.setPersonalName("personal");
+        // authorRepo.save(author);
+        initAuthors();
+        initWorks();
+     //   System.out.println(worksDumpLocation);
+
+    }
+
+    @Bean
     public CqlSessionBuilderCustomizer sessionBuilderCustomizer(DataStaxAstraProperties astraProperties) {
         Path bundle = astraProperties.getSecureConnectBundle().toPath();
         return builder -> builder.withCloudSecureConnectBundle(bundle);
